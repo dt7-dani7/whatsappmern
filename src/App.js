55e419b4a2c338a873dd6205ca484a77d68be706
Danish Thomas <53125171+dt7-dani7@users.noreply.github.com>
2021-04-01 12:47:55 +0530

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Sidebar from "./Sidebar.js";
+import Chat from "./Chat.js";
 function App() {
   return (
-    
+      <div className="app">
+        <div className="app__body">
+        <Sidebar/>
+        <Chat/>
+        </div>
+        
+      </div>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDIXgaWweUmzKVsYfmzSE9--Urs4ndEKyo",
+    authDomain: "whatsapp-mern-ebad8.firebaseapp.com",
+    projectId: "whatsapp-mern-ebad8",
+    storageBucket: "whatsapp-mern-ebad8.appspot.com",
+    messagingSenderId: "28551126967",
+    appId: "1:28551126967:web:c0d890f8505c6601259232",
+    measurementId: "G-9CT4KVDJ0P"
+  };
