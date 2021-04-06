c7a3ef7c469baa78a628a42a257937b444ed6a17
Danish Thomas <53125171+dt7-dani7@users.noreply.github.com>
2021-04-06 13:07:17 +0530

    frontend finished

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,62 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React from 'react'
+import MicIcon from "@material-ui/icons/Mic";
 import "./Chat.css";
 function Chat() {
     return (
         <div className="chat">
-            <h1>This is chat</h1>
+            <div className="chat__header">
+                <Avatar/>
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last Seen</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Dani</span>
+                    This is a message
+                    <span className="chat__timestamp">
+                    {new Date().toUTCString()} 
+                    </span>   
+                </p>
+                <p className="chat__message chat__reciever">
+                    <span className="chat__name">Dani</span>
+                    This is a message
+                    <span className="chat__timestamp">
+                    {new Date().toUTCString()} 
+                    </span>   
+                </p>
+                <p className="chat__message">
+                    <span className="chat__name">Dani</span>
+                    This is a message
+                    <span className="chat__timestamp">
+                    {new Date().toUTCString()} 
+                    </span>   
+                </p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticon/>
+                <form>
+                    <input placehoder="Type a Message" type="text"/>
+                    <button type="submit">
+                        Send a message
+                    </button>
+                </form>
+                <MicIcon />
+            </div>
         </div>
     )
 }
