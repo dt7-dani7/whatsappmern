c37dd687e6b311666d83da5c34ee230cec64d01b
Danish Thomas <53125171+dt7-dani7@users.noreply.github.com>
2021-04-08 11:10:48 +0530

    integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, {useEffect} from 'react';
 import './App.css';
 import Sidebar from "./Sidebar.js";
 import Chat from "./Chat.js";
+import Pusher from "pusher-js";
 function App() {
+  useEffect( () => {
+    const pusher = new Pusher('964d374873f90df5ae9b', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, [])
   return (
       <div className="app">
         <div className="app__body">
