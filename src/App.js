b24c92ec8795e7aa66f7e9e04545e1e9218e3113
Danish Thomas <53125171+dt7-dani7@users.noreply.github.com>
2021-04-08 11:54:02 +0530

    pusher

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Sidebar from "./Sidebar.js";
 import Chat from "./Chat.js";
 import Pusher from "pusher-js";
+import axios from "./axios";
 function App() {
+  const [messages, setMessges] = useState([]);
+
+  useEffect(() => {
+    axios.get('/messages/sync')
+    .then(response => {
+      setMessges(response.data);
+
+    })
+  }, [])
   useEffect( () => {
     const pusher = new Pusher('964d374873f90df5ae9b', {
       cluster: 'ap2'
     });
 
     const channel = pusher.subscribe('messages');
-    channel.bind('inserted', (data) => {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', (newMessage) => {
+      setMessges([...messages, newMessage])
     });
-  }, [])
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+
+  }, [messages]);
+
+  console.log(messages);
+
   return (
       <div className="app">
         <div className="app__body">
